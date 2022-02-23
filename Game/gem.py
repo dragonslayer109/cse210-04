11299439bed608434181d561ce49bb91d7e815cc
dragonslayer109 <raydonvetten@gmail.com>
2022-02-23 12:23:00 +0200

    update on features
    feat - new features added to the documents
    
    inherited actor class from actor
    initiated gem and rock class with respective attributes
    created getters and setters

--- Game/gem.py
+++ Game/gem.py
@@ -1,4 +1,30 @@
-from Game import Point
+import random
+from point import Point
+from actor import Actor
 
-class Gem:
-    pass
+class Gem(Actor):
+    """
+    The visual representation of the Gem in the game. 
+    This class keeps track of the appearance, position and velocity of the Gem.
+
+    Inherits 
+    """
+    def __init__(self):
+        super().__init__()
+        self._gem = "*"
+     
+    def get_gem(self):
+        """Gets the gem's visual appearance.
+        
+        Returns:
+            The gem's visual appearance.
+        """
+        return self._gem
+    
+    def set_gem(self, gem):
+        """Updates the appearance of the gem.
+        
+        Args:
+            gem: The given appearance of the Actor.
+        """
+        self._gem = gem
--- Game/rocks.py
+++ Game/rocks.py
@@ -1,4 +1,30 @@
-from Game import Point
+import random
+from point import Point
+from actor import Actor
 
-class rock:
-    pass
+class Rock(Actor):
+    """
+    The visual representation of the rocks in the game. 
+    This class keeps track of the appearance, position and velocity of the Rock.
+
+    Inherits 
+    """
+    def __init__(self):
+        super().__init__()
+        self._rock = "o"
+
+    def get_rock(self):
+        """Gets the Rock's visual appearance.
+        
+        Returns:
+            The Rock's visual appearance.
+        """
+        return self._rock
+    
+    def set_rock(self, rock):
+        """Updates the appearance of the Actor.
+        
+        Args:
+            rock: The given appearance of the Actor.
+        """
+        self._rock = rock
