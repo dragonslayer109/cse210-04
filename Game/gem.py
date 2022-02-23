33eb6ffce451045b1d73ab0d3146d6f76e6bdf3d
dragonslayer109 <raydonvetten@gmail.com>
2022-02-23 12:47:49 +0200

    features added
    feat - remaining features added to classes
    
    getters and setters added for both rock and gem class position and velocity.
    
    hit method added to keep track of score on both rock and gem class.

--- Game/gem.py
+++ Game/gem.py
@@ -11,6 +11,8 @@ class Gem(Actor):
     """
     def __init__(self):
         super().__init__()
+        self.score = 0
+        self.alive = True
         self._gem = "*"
      
     def get_gem(self):
@@ -25,6 +27,50 @@ class Gem(Actor):
         """Updates the appearance of the gem.
         
         Args:
-            gem: The given appearance of the Actor.
+            gem: The given appearance of the gem.
         """
         self._gem = gem
+    
+    def get_gem_position(self):
+        """Gets the gem's current coordinates.
+        
+        Returns:
+            The gem's current coordinates.
+        """
+        self._position.x = float(random.uniform(-600, 600))
+        self._position.y = 600
+
+    def get_gem_velocity(self):
+        """Gets the gem's current speed and direction.
+        
+        Returns:
+            The gem's current speed and direction.
+        """
+        return self._velocity
+
+    def set_gem_position(self, position):
+        """Updates the coordinates of the gem.
+        
+        Args:
+            position: The given coordinates of the gem.
+        """
+        self._position = position
+
+    def set_gem_velocity(self, velocity):
+        """Updates the speed and direction of the gem.
+        
+        Args:
+            velocity: The given speed and direction of the gem.
+        """
+        self._velocity = velocity
+
+    def hit(self):
+        """
+        Sets gem to false when hit by player and adds a score
+
+        Returns:
+            The points scored
+        """
+        self.alive = False
+        self.score = 1
+        return self.score
--- Game/rocks.py
+++ Game/rocks.py
@@ -11,6 +11,7 @@ class Rock(Actor):
     """
     def __init__(self):
         super().__init__()
+        self.alive = True
         self._rock = "o"
 
     def get_rock(self):
@@ -22,9 +23,53 @@ class Rock(Actor):
         return self._rock
     
     def set_rock(self, rock):
-        """Updates the appearance of the Actor.
+        """Updates the appearance of the rock.
         
         Args:
-            rock: The given appearance of the Actor.
+            rock: The given appearance of the rock.
         """
         self._rock = rock
+
+    def get_rock_position(self):
+        """Gets the rock's current coordinates.
+        
+        Returns:
+            The rock's current coordinates.
+        """
+        self._position.x = float(random.uniform(-600, 600))
+        self._position.y = 600
+
+    def get_rock_velocity(self):
+        """Gets the rock's current speed and direction.
+        
+        Returns:
+            The rock's current speed and direction.
+        """
+        return self._velocity
+
+    def set_rock_position(self, position):
+        """Updates the coordinates of the rock.
+        
+        Args:
+            position: The given coordinates of the rock.
+        """
+        self._position = position
+
+    def set_rock_velocity(self, velocity):
+        """Updates the speed and direction of the rock.
+        
+        Args:
+            velocity: The given speed and direction of the rock.
+        """
+        self._velocity = velocity
+
+    def hit(self):
+        """
+        Sets rock to false when hit by player and adds a score
+
+        Returns:
+            The points scored
+        """
+        self.alive = False
+        self.score = -1
+        return self.score
